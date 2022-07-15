f3736ff436ad4054a2ec828eb95f3aa6dbe96184
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-07-15 14:10:01 -0300

    função

--- JS/3- Manipulando o DOM/-robotron-2000-projeto_inicial/script.js
+++ JS/3- Manipulando o DOM/-robotron-2000-projeto_inicial/script.js
@@ -1,8 +1,20 @@
-/*document.querySelector('.robo').onclick = function () {
-    alert('oi');
-}*/
+let controle = document.querySelectorAll('.controle-ajuste');
 
-document.querySelector('.robo').addEventListener('click', () => {
-    alert('oi');
-} );
+controle.forEach((elemento) => { //forEach chama uma função para cada elemento dentro do array
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+    });
+});
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('.controle-contador');
+
+    if (operacao == '-') {
+        peca.value = Number(peca.value) - 1;
+    } else {
+        peca.value = Number(peca.value) + 1;
+    }
+}
+
+
 
