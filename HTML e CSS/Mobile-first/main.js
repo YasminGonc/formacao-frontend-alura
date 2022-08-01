54948fd604af67d612573ac5b7b129f494c585a4
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-01 19:57:50 -0300

    responsividade tablet

--- HTML e CSS/Mobile-first/main.js
+++ HTML e CSS/Mobile-first/main.js
@@ -5,6 +5,12 @@ btnMenu.addEventListener('click', () => {
     listaMenu.classList.toggle('on');
 });
 
+const btnMenuCategorias = document.querySelector(".opcoes__item-menu");
+
+btnMenuCategorias.addEventListener('click', () => {
+    listaMenu.classList.toggle('on');
+});
+
 const swiper = new Swiper('.swiper', {
     spaceBetween: 10,
     slidesPerView: 3,
