4e7885901b7153cd44ab566cb308d89197ef9b9f
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-25 07:36:17 -0300

    encerramento módulo 1

--- TS/Parte 1/app/controllers/negociacao-controller.ts
+++ TS/Parte 1/app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
 import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -13,9 +15,10 @@ export class NegociacaoController {
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
+        this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
-
-        console.log(negociacao);
+        
+        console.log(this.negociacoes.lista());
     }
 
     criaNegociacao(): Negociacao {
