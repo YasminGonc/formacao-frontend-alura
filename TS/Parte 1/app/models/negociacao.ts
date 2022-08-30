d9e31b94419149a86dc1e18c6b163ade9c4429e5
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2022-08-29 22:15:50 -0300

    finalização formação

--- TS/Parte 1/app/models/negociacao.ts
+++ TS/Parte 1/app/models/negociacao.ts
@@ -22,4 +22,10 @@ export class Negociacao {
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
     }
+
+    public ehIgual(negociacao: Negociacao): boolean {
+        return this.data.getDate() === negociacao.data.getDate() 
+        && this.data.getMonth() === negociacao.data.getMonth() 
+        && this.data.getFullYear() === negociacao.data.getFullYear(); 
+    }
 }
